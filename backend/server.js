a01ac547a43fb2282b5f73aad59e38ef23c45813
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-09-09 09:19:11 +0300

    creating server.js file that contains the core of our backend

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const dbConnect = require('./config/dbConnect')
+const userRoutes = require('./routes/userRoutes')
+const bodyParser = require('body-parser')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const cors = require('cors');
+const expenseRoutes = require('./routes/expenseRoutes');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+dbConnect();
+
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false, 
+    saveUninitialized: false, 
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_SESSION_URI, 
+        collectionName: 'sessions'
+    }), 
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/api/users', userRoutes);
+app.use('/api/expenses', expenseRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
