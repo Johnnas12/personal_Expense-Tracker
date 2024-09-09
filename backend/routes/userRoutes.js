f873ccdc2a1e6a3254bf105264523402365a1455
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-09-09 09:35:14 +0300

    adding the routes for login and logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { registerUser } = require('../controllers/userController');
-
+const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 
 // Authentication and authorization routes
 router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', authenticateToken, logoutUser);
 
 module.exports = router;
