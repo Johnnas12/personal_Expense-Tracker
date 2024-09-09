30b9562009b66e64ee0e533323c1a0ac3f39f042
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-09-09 09:40:07 +0300

    importing the login and logout functions from my userController

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser, logoutUser } = require('../controllers/userController');
 const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 
 // Authentication and authorization routes
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/login',  loginUser);
 router.post('/logout', authenticateToken, logoutUser);
 
 module.exports = router;
